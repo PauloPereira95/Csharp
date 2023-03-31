6e763d77d4bcaa2fc78557f25799f7d77a33d79d
Paulo Pereira <paulohuawei95@gmail.com>
2023-03-31 22:57:57 +0100

    exe 6.2

---
+++ Pseudocodigos/exe 6.2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exe_6._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number;
+            Console.WriteLine("Type the number to check:");
+            number = int.Parse(Console.ReadLine());
+            if (number < 0)
+            {
+                Console.WriteLine("Number is Negative !");
+            } else if (number > 0)
+            {
+                Console.WriteLine("Number is Positive !");
+            } else
+            {
+                Console.WriteLine("Number is Zero !");
+            }
+            Console.ReadLine();
+        }
+    }
+}
