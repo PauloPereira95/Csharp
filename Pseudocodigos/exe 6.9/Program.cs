d8df9be8eac693e2ca0e435d95c09c4a6e33ea79
Paulo Pereira <paulohuawei95@gmail.com>
2023-04-01 19:58:47 +0100

    exe 6.9 multiple of 10

---
+++ Pseudocodigos/exe 6.9/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exe_6._9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i=1; i <= 100; i++) {
+                // check if is multiple of 10, devide by 10
+                if (i % 10 == 0)
+                {
+                    Console.WriteLine($"Multiplo de 10 : {i}");
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
