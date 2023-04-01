d28bcc2a8876ec3b7cec9241992524a6e763c8e2
Paulo Pereira <paulohuawei95@gmail.com>
2023-04-01 15:03:16 +0100

    exe 6.4

---
+++ Pseudocodigos/exe  6.4 soma_sub_div/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exe__6._4_soma_sub_div
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
