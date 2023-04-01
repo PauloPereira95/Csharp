b2f8eb4976d2a57270c84d56160ff6ec20ffe90d
Paulo Pereira <paulohuawei95@gmail.com>
2023-04-01 19:45:52 +0100

    exe 6.8

---
+++ Pseudocodigos/exe6.8/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exe6._8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i = 100; i > 0; i--)
+            {
+                Console.WriteLine(i);
+            }
+            Console.ReadLine();
+        }
+    }
+}
