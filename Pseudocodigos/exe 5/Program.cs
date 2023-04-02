4d032fd91b93b42e4fcce8fc842fe0d7fb304e47
Paulo Pereira <paulohuawei95@gmail.com>
2023-04-02 15:13:25 +0100

    exe 5

---
+++ Pseudocodigos/exe 5/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exe_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double valOne, valTwo, valThree,avg = 0;
+            // ask for the values
+            Console.WriteLine("Type the First Value : ");
+            valOne = double.Parse(Console.ReadLine());
+            Console.WriteLine("Type the second value : ");
+            valTwo = double.Parse(Console.ReadLine());
+            Console.WriteLine("Type the third value : ");
+            valThree = double.Parse(Console.ReadLine());
+            // calculate the math
+            avg = ((valOne * 5) + (valTwo * 3) + (valThree * 2)) / 10;
+            //show result
+            Console.WriteLine($"The Average is -> {avg}");
+            Console.ReadLine();
+        }
+    }
+}
