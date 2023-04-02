e32faf40ffcbeaee4b035b7f6b9574682fb44c60
Paulo Pereira <paulohuawei95@gmail.com>
2023-04-02 19:44:24 +0100

    add comments

---
+++ Pseudocodigos/exe 8/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exe_8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int age;
+            // ask age
+            Console.WriteLine("Tell me, whats your age ? ");
+            age = int.Parse(Console.ReadLine());
+            // check if as adult or not
+            if (age >= 18)
+            {
+                Console.WriteLine("You are a adult");
+            } else
+            {
+                Console.WriteLine("You are a minor");
+            }
+            Console.ReadLine();
+        }
+    }
+}
