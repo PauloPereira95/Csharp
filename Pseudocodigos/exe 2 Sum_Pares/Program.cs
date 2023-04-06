a1ce84393c1801dc3228c2aa9e42fd9b29e13e51
Paulo Pereira <paulohuawei95@gmail.com>
2023-04-06 21:37:59 +0100

    exe 3

--- Pseudocodigos/exe 2 Sum_Pares/Program.cs
+++ Pseudocodigos/exe 2 Sum_Pares/Program.cs
@@ -4,8 +4,23 @@
     {
         static void Main(string[] args)
         {
+            try
+            {
+                double i = 0, sum = 0, val;
+                Console.WriteLine("Type the value to sum the pairs ");
+                val = double.Parse(Console.ReadLine());
 
+                while (i <= val)
+                {
+                    sum += i;
+                    i += 2;
+                }
+                Console.WriteLine($"Total -> {sum}");
 
+            } catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
         }
     }
 }
