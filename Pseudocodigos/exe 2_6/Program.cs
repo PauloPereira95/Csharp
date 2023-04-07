0af05d64ad98b64908074114308350eeb5757dfa
Paulo Pereira <paulohuawei95@gmail.com>
2023-04-07 15:11:34 +0100

    find multiples  of 2 , 3 ,5

---
+++ Pseudocodigos/exe 2_6/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace exe_2_6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i <= 100;i++)
+            {
+                if ( i % 2 == 0 && i % 3 == 0 && i % 5 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
